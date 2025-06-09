ea38a9f77e88892275deb71772f03d3975d986ce
Gabriel Deva Caniyago <111201911994@mhs.dinus.ac.id>
2025-06-09 19:25:28 +0700

    update baru read data & validation

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -5,22 +5,28 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
+use App\Models\UserModel;
+
 class AuthController extends BaseController
 {
+    protected $user;
+
     function __construct()
     {
-    helper('form');
+        helper('form');
+        $this->user= new UserModel();
     }
+    
     public function login()
 {
     if ($this->request->getPost()) {
         $username = $this->request->getVar('username');
         $password = $this->request->getVar('password');
 
-        $dataUser = ['username' => 'desember', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin']; // passw 123
+        $dataUser = $this->user->where(['username' => $username])->first(); // passw 1234567 
 
-        if ($username == $dataUser['username']) {
-            if (md5($password) == $dataUser['password']) {
+        if ($dataUser) {
+            if (password_verify($password, $dataUser['password'])) {
                 session()->set([
                     'username' => $dataUser['username'],
                     'password' => $dataUser['password'],
