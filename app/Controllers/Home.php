e84be40a3653b087f3692f4ec5b2c5baebbcc81e
Gabriel Deva Caniyago <111201911994@mhs.dinus.ac.id>
2025-06-30 13:27:55 +0700

    menambahkan data transaksi pembelian

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,17 +2,24 @@
 
 namespace App\Controllers;
 
-use App\Models\ProductModel; 
+use App\Database\Migrations\Transaction;
+use App\Database\Migrations\TransactionDetail;
+use App\Models\ProductModel;
+use App\Models\TransactionModel;
+use app\Models\TransactionDetailModel; 
 
 class Home extends BaseController
 {
     protected $product;
+    protected $transaction;
+    protected $transaction_detail;
 
     function __construct()
     {
         helper('form');
         helper('number');
         $this->product = new ProductModel();
+        
     }
     
     public function index()
